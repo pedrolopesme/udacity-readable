aeac22d1d21f4e99fab802b044e1c6e42f176410
Pedro Mendes <pedrolopesme@gmail.com>
2018-12-08 19:09:56 -0200

    minor: render to post element all content request by the project

--- frontend/src/components/PostComponent.js
+++ frontend/src/components/PostComponent.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Link } from 'react-router-dom'
 
 class PostComponent extends Component {
     render = () => {
         const post = this.props.post;
-        return <div>
+        return <Fragment>
             {post && (
-                <h3> {post.title} </h3>
+                <div>
+                    <h3> {post.title} </h3>
+                    <div>
+                        {post.body}
+                    </div>
+                    <div>
+                        <p> <small> By: {post.author} </small> </p>
+                        <p> <small> # Comments: {post.commentCount} </small> </p>
+                        <p> <small> Score: {post.voteScore} </small> </p>
+                        <p>
+                            <button> Up Vote </button> 
+                            <button> Down Vote </button>
+                        </p>
+                        <p>
+                            <Link to={`/`}> Edit </Link> |
+                            <Link to={`/`}> Delete </Link>
+                        </p>
+                    </div>
+                </div>
             )}
-        </div>
+        </Fragment>
     }
 }
 
