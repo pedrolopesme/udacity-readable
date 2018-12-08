b8ec665aab5a2b46e51ceccf73ea07ad431525a8
Pedro Mendes <pedro.mendes@peixeurbano.com>
2018-12-08 17:36:10 -0200

    bugfix: add key attr to element in map

--- frontend/src/containers/PostsContainer.js
+++ frontend/src/containers/PostsContainer.js
@@ -8,12 +8,12 @@ class PostsContainer extends Component {
         return <div>
             <h1> POSTS </h1>
             { this.props.posts && Object.keys(this.props.posts).map( key =>
-                <PostComponent post={this.props.posts[key]}/> 
+                <PostComponent key={key} post={this.props.posts[key]}/> 
             )}
 
              <h1> CATEGORIES </h1>
             { this.props.categories && Object.keys(this.props.categories).map( key =>
-                <CategoryComponent category={this.props.categories[key]}/> 
+                <CategoryComponent key={key} category={this.props.categories[key]}/> 
             )}
         </div>
     }
