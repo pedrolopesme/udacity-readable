2679c26b6163a0336ad88972a74136d4f98bf83b
Pedro Mendes <pedrolopesme@gmail.com>
2018-12-08 18:42:21 -0200

    refactor: extract code responsible for pick the right post to render from props on PostContainer

--- frontend/src/containers/PostContainer.js
+++ frontend/src/containers/PostContainer.js
@@ -2,28 +2,31 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
 class PostContainer extends Component {
-    render () {
-        const id = this.props.match.params.id;
-        const posts = this.props.posts;
-        let post;
+    // filterPost knows how to extract the right post
+    // to render on PostContainer
+    filterPost = (props) => {
+        const id = props.match.params.id;
+        const posts = props.posts;
 
-        if(id && posts) {
-            post = Object.keys(posts)
-                        .map( key => posts[key])
-                        .filter(p => p.id === id)
-                        .shift();
-        } 
+        if (id && posts) {
+            return Object.keys(posts)
+                .map(key => posts[key])
+                .filter(p => p.id === id)
+                .shift();
+        }
+    }
 
+    render() {
+        const post = this.filterPost(this.props);
         return <div>
             {post && (
-                <h1> {post.title} </h1>
+                <h3> {post.title} </h3>
             )}
-            
         </div>
     }
 }
 
-function mapStateToProps({Posts}){
+function mapStateToProps({ Posts }) {
     return {
         posts: Posts,
     }
